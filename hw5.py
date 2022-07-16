92fbce17e52cfa3c02a2c5cfcf6728abab9e8b83
darianYTKA <109409574+darianYTKA@users.noreply.github.com>
2022-07-16 11:31:04 +0400

    Add files via upload

---
+++ hw5.py
@@ -0,0 +1,67 @@
+# Name:     Darian Lee
+# SID:      918181338
+# Section:  A01
+
+class BinHeap:
+    def __init__(self):
+        self.heapList = [0]
+        self.currentSize = 0
+
+    def percUp(self,i):
+        while i // 2 > 0:
+          if self.heapList[i] < self.heapList[i // 2]:
+             tmp = self.heapList[i // 2]
+             self.heapList[i // 2] = self.heapList[i]
+             self.heapList[i] = tmp
+          i = i // 2
+
+    def insert(self,k):
+      self.heapList.append(k)
+      self.currentSize = self.currentSize + 1
+      self.percUp(self.currentSize)
+
+    def percDown(self,i):
+      while (i * 2) <= self.currentSize:
+          mc = self.minChild(i)
+          if self.heapList[i] > self.heapList[mc]:
+              tmp = self.heapList[i]
+              self.heapList[i] = self.heapList[mc]
+              self.heapList[mc] = tmp
+          i = mc
+
+    def minChild(self,i):
+      if i * 2 + 1 > self.currentSize:
+          return i * 2
+      else:
+          if self.heapList[i * 2] < self.heapList[i * 2 + 1]:
+              return i * 2
+          else:
+              return i * 2 + 1
+
+    def delMin(self):
+      retval = self.heapList[1]
+      self.heapList[1] = self.heapList[self.currentSize]
+      self.currentSize = self.currentSize - 1
+      self.heapList.pop()
+      self.percDown(1)
+      return retval
+
+    def buildHeap(self, alist):
+      i = len(alist) // 2
+      self.currentSize = len(alist)
+      self.heapList = [0] + alist[:]
+      while (i > 0):
+          self.percDown(i)
+          i = i - 1
+
+# TODO
+def heapsort(python_list):
+    heapım = BinHeap() #adding "im" or "ım" to the end of the word is how you say "my" in turkish.   
+    heapım.buildHeap(python_list)
+    listim = []
+    while len(listim) < len(python_list): #This would be so much simpliler if you could access local variables like the size of the heap 
+        listim.append(heapım.delMin())
+    return listim
+
+mylist=[0,9,8,7,6,80,8,7,56,8]
+print(heapsort(mylist))
